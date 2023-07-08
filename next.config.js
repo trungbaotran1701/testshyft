df3c8974543d4f4fa4c3241983011fa55514a426
trungbaotran1701 <trungbaotran1701@gmail.com>
2023-07-09 03:54:26 +0700

    change key and add some config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    
+  }
+  
+  module.exports = nextConfig
+  
+  module.exports = {
+  
+    env: {
+      APIKEY:"KJG-wyaBq6NGdkIz",
+      PRIVATE_KEY_WALLET:"0x07bb8829d8dd4f2d92b6369e15945da6cbea4c1ddb38f2a2559282649c482279"
+    },
+  }
